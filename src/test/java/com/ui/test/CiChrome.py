bc38df0923b17b0199f5c96004d301d4af499572
PATILPX25 <breakingbadjessepinkmanww@gmail.com>
2025-12-08 12:06:36 +0530

    Create CiChrome.py

---
+++ src/test/java/com/ui/test/CiChrome.py
@@ -0,0 +1,30 @@
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class CiChrome {
+    public static void main(String[] args) {
+        // Verbose driver log
+        System.setProperty("webdriver.chrome.logfile", "chromedriver.log");
+        System.setProperty("webdriver.chrome.verboseLogging", "true");
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(options);
+        try {
+            driver.get("https://example.com");
+            System.out.println("Title: " + driver.getTitle());
+        } finally {
+            driver.quit();
+        }
+    }
+}
